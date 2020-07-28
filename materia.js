86be97a5ce2450d9bd2a360970f6911c5cc33eaa
marheusmatos <marheusmatos@gmail.com>
2020-07-28 01:51:51 -0300

    Retirando código repetido

--- materia.js
+++ materia.js
@@ -9,14 +9,26 @@ class materia {
     }
 
 }
-
+ 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
 var nome;
 var materias = new Array(4);
-var cor;
-
+var cor = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 var seg, ter, qua, qui, sex;
-
 var i = 0;
 function adicionar() {
 
@@ -28,15 +40,12 @@ function adicionar() {
     qui = document.getElementById("qui").value.split(",");
     sex = document.getElementById("sex").value.split(",");
 
-    cor = "rgb(" + (Math.floor(Math.random() * 204) + 50) + "," + (Math.floor(Math.random() * 204) + 50) + "," + (Math.floor(Math.random() * 204) + 50) + ");";
-
     materias[i] = new materia(nome, cor, seg, ter, qua, qui, sex);
-    document.getElementById("box").innerHTML += "<div id='card3' style='background-color: " + cor + ";' class='four columns'> " + materias[i].nome + "<br>" + materias[i].seg + "</div>";
+    document.getElementById("box").innerHTML += "<div id='card3' style='background-color: " + cor[getRandomInt(0,50)] + ";' class='four columns'> " + materias[i].nome + "<br>" + materias[i].seg + "</div>";
     i++;
 }
 
 var horarioIguais = 0;
-var score=0;
 //true quando dois valores são iguais
 function checarIgual(array1, array2) {
 
@@ -51,57 +60,56 @@ function checarIgual(array1, array2) {
                 return true;
             }
         }
-
     }
-
 }
 
 function gerar() {
-
-    var cor = "rgb(" + (Math.floor(Math.random() * 184) + 70) + "," + (Math.floor(Math.random() * 184) + 70) + "," + (Math.floor(Math.random() * 184) + 70) + ");";
 
     materias[0] = new materia("a", "cor", [[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]]);
     materias[1] = new materia("b", "cor", [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2]]);
     materias[2] = new materia("c", "cor", [[3, 4], [7, 8], [7, 8], [7, 8], [7, 8]]);//3 4
     materias[3] = new materia("d", "cor", [[5, 6], [5, 6], [5, 6], [5, 6], [5, 6]]);
     materias[4] = new materia("e", "cor", [[3, 4], [3, 4], [3, 4], [3, 4], [3, 4]]);
-   
+
     for (var x = 0; x < materias.length; x++) {
-        var text=" <div id='card3' style='background-color:" + cor + "'>";
-        
-        
-        
-        
+
+        var text = "";
+        var atributes = x.toString();
+
+
+
         for (var y = 0; y < materias.length; y++) {
             if (y > x) {
                 for (var z = 0; z < materias.length; z++) {
                     checarIgual(materias[x].dias[z], materias[y].dias[z]);
-                
-                }
 
+                }
 
                 // se não tiver nenhum horario que se repete
                 if (horarioIguais == 0) {
+                    atributes += "," + y;
 
-                   text += materias[x].nome + " com " + materias[y].nome + "| Aulas: " + materias[x].dias + " ; " + materias[y].dias + "<br>";
+                    text += materias[x].nome + " com " + materias[y].nome + "| Aulas: " + materias[x].dias + " ; " + materias[y].dias + "<br>";
 
                 }
-                
+
                 horarioIguais = 0;
             }
-        };
-        
+        }
+
         // quando a aula mudar
-        document.getElementById("teste").innerHTML += text +  "</div>";
-        
+
+        // adiciona uma div com a estrutura = começo da tag  + texto do meio + fechamento da tag 
+
+        document.getElementById("teste").innerHTML += "<div id='card3' style='background-color:" + cor[getRandomInt(0,50)] + "' onclick='gerarTabela([" + atributes + "])'>" + text + "</div>";
+
         total = 0;
-        cor = "rgb(" + (Math.floor(Math.random() * 204) + 50) + "," + (Math.floor(Math.random() * 204) + 50) + "," + (Math.floor(Math.random() * 204) + 50) + ");";;
-        text=" <div id='card3' background-color:'" + cor + "'>";
+        text = " <div id='card3' background-color:'" + cor + "'>";
     }
 
-
 }
-function gerarTabela(){
 
+function gerarTabela(x) {
+    alert(x.length);
 
 }
